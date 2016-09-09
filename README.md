8ae4cb5b959c7d23ec397ee6e6678e65c32631d2
Josh Suarez <jsuarez@varickmm.com>
2016-09-09 15:45:07 -0400

    First commit.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # greasemonkey_scripts
 Greasemonkey scripts
+
+* __Yelp sort__ - redirects you to a "Newest First" listing of reviews on a Yelp page.
---
+++ yelp_sort_by_date.js
@@ -0,0 +1,20 @@
+// ==UserScript==
+// @name        Sort Yelp By Date
+// @namespace   yelp-sort
+// @description Automatically sort any yelp page reviews by date (instead of fishy yelp sort)
+// @include     *.yelp.com/*
+// @exclude     *.yelp.com/*?sort_by=date_desc
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+//Get the current url
+var curUrl = window.location.toString();
+
+//Just in case the exclude didnt work
+if(curUrl.indexOf("?sort_by=date_desc") > -1){
+  return;
+}
+//move to the date_desc search
+else
+  window.location.href = curUrl + "?sort_by=date_desc";
