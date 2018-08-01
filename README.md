9d5e3fbfb14549de687deb02577ff2e3d1474abc
Josh Suarez <joshuasuarez@gmail.com>
2018-07-31 21:46:37 -0400

    Added Block Youtube Channels script

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-# greasemonkey_scripts
+B# greasemonkey_scripts
 Greasemonkey scripts
 
 * __Yelp Sort By Date__ - redirects you to a "Newest First" listing of reviews on a Yelp page, rather than the default "Yelp Sort".
 
 * __Remove Youtube Frontpage Videos__ - Removes all suggested and trending videos (many of the thumbnails are clickbait or just plain disturbing) on the frontpage of Youtube.
+
+* __Block Youtube Channels__ - Remove results from unwanted channels on the Youtube search results page.
---
+++ block_youtube_channels.user.js
@@ -0,0 +1,42 @@
+/ ==UserScript==
+// @name     Block Youtube Channels
+// @description Remove unwanted channels from appearing in youtube search results
+// @version  1
+// @include *.youtube.com/results*
+// @grant    none
+// ==/UserScript==
+
+var resultsListSelect = "#contents.ytd-item-section-renderer",
+    videoRenderDivSelect = "div#dismissable",
+    channelsToRemove = ['FungBrosComedy','TheYoungTurks','TYTInterviews'];
+
+//Build the query string of channels to remove
+var queryStringStart = "a[href*='", 
+    queryStringEnd = "' i]",
+allQueryStrings = [],
+    completeQueryString = "";
+for(var j = 0; j < channelsToRemove.length; j++){
+  allQueryStrings.push( queryStringStart + channelsToRemove[j] + queryStringEnd );
+}
+completeQueryString = allQueryStrings.join(", ");
+
+
+//Callback function
+function removeUnwantedVideos(){
+  var userLink = document.querySelectorAll(completeQueryString);
+  if(userLink.length > 0){
+      for(var i = 0; i < userLink.length; i++){
+      var dismissable = userLink[i].closest(videoRenderDivSelect);
+      if(dismissable == null)
+        continue;
+      
+	  var renderDiv = dismissable.parentNode;
+      renderDiv.parentNode.removeChild(renderDiv);
+    }
+  }
+}
+
+//Begin observing the results list
+var resultsDiv = document.querySelector(resultsListSelect);
+var observer = new MutationObserver(removeUnwantedVideos);
+observer.observe(resultsDiv, {childList: true});
