657ed7dd608aabbff59e96a73b2155327dd9b6c7
Joshua Suarez <joshua.suarez@equinox.com>
2020-03-17 13:45:37 -0400

    Added ability to 'ignore' usrs as well to Slack Taco Reader.

--- slack_taco_reader.user.js
+++ slack_taco_reader.user.js
@@ -1,13 +1,14 @@
 // ==UserScript==
 // @name        Slack Taco Reader
 // @namespace   slack-taco
-// @description Mark :taco: messages as read automatically. Requires the "All Unreads" feature enabled.
+// @description Mark :taco: messages and bot messages as read automatically. Requires the "All Unreads" feature enabled.
 // @include     *.slack.com/client/*
 // @exclude     none
-// @version     1
+// @version     2
 // @grant       none
 // ==/UserScript==
 
+timesMarkedRead = 0;
 
 (function(){
 	//Check every x seconds
@@ -45,21 +46,42 @@ function checkForUnreads(){
 		showNew[0].click();
 	}
 
-	let plainMessages = document.getElementsByClassName('c-message_kit__text');
-	if(plainMessages.length > 0){
-		console.log("Non-Taco message found, leaving unread...");
+	//----------------------------------------------------
+	//Checking for ignored user messages. Only marks read if only ignored users are found.
+	let senders = document.getElementsByClassName('c-message__sender_link');
+	let allSpamMsgs = false;
+	for(let i = 0; i < senders.length; i++){
+		allSpamMsgs = checkIgnoredUser(senders[i].innerHTML);
+	}
+
+	if(allSpamMsgs){
+		console.log("Taco_Reader: Only ignored user messages found, marking read...(" + (++timesMarkedRead) + ")");
+		let markReadButton = document.getElementsByClassName('c-button--outline')[0];
+		markReadButton.click();
 		resetCursor();
 		return;
 	}
 
+	//----------------------------------------------------
+	//Checking if there are plain text messages. If so, no tacos, leave them unread
+	let plainMessages = document.getElementsByClassName('c-message_kit__text');
+	if(plainMessages.length > 0){
+		console.log("Taco_Reader: Non-Taco message found, leaving unread...");
+		resetCursor();
+		return;
+	}
+
+	//----------------------------------------------------
+	//Check rich text messages for tacos. If there are nothing but tacos, mark all as read.
 	let messages = document.getElementsByClassName('p-rich_text_section');
 	let nonTacoMsgFound = false;
 	for(let i = 0; i < messages.length; i++){
 		let childNodes = messages[i].children;
 		let foundTaco = false;
 		for(let j = 0; j < childNodes.length; j++){
-			if(childNodes[j].outerHTML.indexOf(':taco:') > -1){
-				foundTaco = true;
+			foundTaco = checkIgnoredContent(childNodes[j].outerHTML);
+			if(foundTaco){
+				break;
 			}
 		}
 		if(!foundTaco){
@@ -68,10 +90,10 @@ function checkForUnreads(){
 	}
 
 	if(nonTacoMsgFound){
-		console.log("Non-Taco message found, leaving unread...");
+		console.log("Taco_Reader: Non-Taco message found, leaving unread...");
 	}
 	else{
-		console.log("Only taco messages found, marking read...");
+		console.log("Taco_Reader: Only taco messages found, marking read...(" + (++timesMarkedRead) + ")");
 		let markReadButton = document.getElementsByClassName('c-button--outline')[0];
 		markReadButton.click();
 	}
@@ -83,4 +105,23 @@ function resetCursor(){
 	//This is to reset the cursor position. If the browser stays on 'All unreads', they won't clear even if read on phone or app.
 	let threadsButton = document.getElementsByClassName('p-channel_sidebar__link--all-threads')[0];
 	threadsButton.click();
+}
+
+function checkIgnoredContent(msg){
+	let ignoreContent = [':taco:', ':sadtaco:'];
+	for(let i = 0; i < ignoreContent.length; i++){
+		if(msg.indexOf(ignoreContent[i]) > -1){
+			return true;
+		}
+	}
+}
+
+function checkIgnoredUser(username){
+	let ignoreUsers = [/*Places user names as strings here*/];
+	for(let i = 0; i < ignoreUsers.length; i++){
+		if(username.toLowerCase() == ignoreUsers[i].toLowerCase()){
+			return true;
+		}
+	}
+	return false;
 }
